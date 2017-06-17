2c74ffe74d4bc61687c42e7c775992486efe01d2
wpank <will.pankiewicz@gmail.com>
2017-06-16 20:07:50 -0500

    Added socket connectino to js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,10 +12,14 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-
+import {Socket, Presence} from "phoenix"
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+let user = documetn.getElementById("User").innerText
+let socket = new Socket("/socket", {params: (user: user)})
+socket.connect()
